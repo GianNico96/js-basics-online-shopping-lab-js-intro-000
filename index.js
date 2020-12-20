e111527b1cf2858556c7e05769568280eda5f453
looping-torrent-8105 <gianherrera96@gmail.com>
2020-12-20 21:35:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,27 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.floor(Math.random() * 100) + 1;
+ cart.push({itemName: item, itemPrice: price});
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  const itemsAndPrices = []
+
+  if(cart.length === 0){
+      console.log('Your shopping cart is empty.');
+    }
+    cart.forEach(elemento => {
+    itemsAndPrices.push(`${elemento.itemName} at $${elemento.itemPrice}`)});
+
+    var myString = 'In your cart, you have'
+    if (itemsAndPrices.length === 1) {
+      myString += itemsAndPrices + '.'
+    }else if (itemsAndPrices === 2) {
+      myString += (itemsAndPrices[0] + 'and' + itemsAndPrices[1] + '.')
+    }
+   return console.log(myString`${itemsAndPrices.join(', ')}`);
 }
 
 function total() {
